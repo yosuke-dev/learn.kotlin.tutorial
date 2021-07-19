2fe1f7dfd1a4835c77a344bf87ce73d50184ea2f
yosuke_dev <devtoku.yosuke@gmail.com>
2021-07-19 23:40:23 +0900

    update coroutine scope builder

--- src/routes/coroutine.kt
+++ src/routes/coroutine.kt
@@ -7,19 +7,20 @@ import io.ktor.routing.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.lang.StringBuilder
 
 fun Route.coroutineRoute() {
     get("/coroutine") {
         val respondText = StringBuilder()
 
-        GlobalScope.launch {
-            delay(1000L)
-            respondText.append("<div>Yosuke.</div>")
+        runBlocking {
+            launch {
+                delay(1000L)
+                respondText.append("<div>Yosuke.</div>")
+            }
+            respondText.append("<div>My name is </div>")
         }
-
-        respondText.append("<div>My name is </div>")
-        Thread.sleep(2000L)
 
         call.respondText(respondText.toString(), contentType = ContentType.Text.Html)
     }
